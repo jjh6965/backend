f2e5be88cf06984db069b0e6107d837bf49cdeec
jjh6965 <jaehun6965@gmail.com>
2025-07-30 16:48:19 +0900

    250730_네이버맵크로스오리진

--- src/main/java/com/boot/cms/controller/map/NaverMapController.java
+++ src/main/java/com/boot/cms/controller/map/NaverMapController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = "https://jh9695.github.io")
 @RestController
 @RequestMapping("/api/naver")
 public class NaverMapController {
