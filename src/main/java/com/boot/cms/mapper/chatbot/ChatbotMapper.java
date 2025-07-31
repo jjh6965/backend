a85609f7abc6f98e4b6bcd2b360e96dc988b52d0
jjh6965 <jaehun6965@gmail.com>
2025-07-31 14:47:58 +0900

    ㅁㅁ

--- src/main/java/com/boot/cms/mapper/chatbot/ChatbotMapper.java
+++
@@ -1,8 +0,0 @@
-package com.boot.cms.mapper.chatbot;
-
-import org.apache.ibatis.annotations.Select;
-
-public interface ChatbotMapper {
-    @Select("Select response FROM chatbot_responses WHERE keyword = #{keyword}")
-    String findResponseByKeyword(String keyword);
-}
